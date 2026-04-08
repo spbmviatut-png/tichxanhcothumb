760d2fc77e84d64b5b82d4bfb14de3dcef85469d
spbmviatut-png <sp.bmviatut@gmail.com>
2026-04-08 19:36:13 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 
-const TOKEN = '8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw';
-const CHAT_ID = '-1003257858837';
+const TOKEN = '8142302072:AAFxGubMrOKkYA7nVzieFmIEZrtdIOLjYHY';
+const CHAT_ID = '6170477038';
 
 const POST = async (req: NextRequest) => {
     try {
